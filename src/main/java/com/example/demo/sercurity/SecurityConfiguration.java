37f9e4a52ccf96ff8e8256415098053b2a1e174d
BuiNgocHa-JC <ngochabui1511@gmail.com>
2020-11-03 17:10:19 +0700

    Spring boot revervation, waiting for GG Calendar

---
+++ src/main/java/com/example/demo/sercurity/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+//package com.example.demo.sercurity;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+//}
